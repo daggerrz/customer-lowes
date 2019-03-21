587cec2a20083a1838fc87a7605be8a0a98e2ef4
Dag Liodden <dag@motiion.com>
2019-03-20 20:05:01 -0400

    Test parsing

--- src/main/kotlin/com/motiion/customer/lowes/Library.kt
+++ src/main/kotlin/com/motiion/customer/lowes/Library.kt
@@ -3,6 +3,8 @@ package com.motiion.customer.lowes
 import org.apache.daffodil.japi.Daffodil
 import org.apache.daffodil.japi.infoset.JDOMInfosetOutputter
 import org.apache.daffodil.japi.io.InputSourceDataInputStream
+import org.jdom2.output.Format
+import org.jdom2.output.XMLOutputter
 import java.io.File
 
 fun main(args: Array<String>) {
@@ -32,11 +34,32 @@ fun main(args: Array<String>) {
     // Parse - parse data to XML
     //
     println("**** Parsing data into XML *****")
-    val file = File("/Users/dag/devel/3p/IBM4690-TLOG/SA/TestData_SA/EAMTRANA_2.DAT")
+//    val file = File("/Users/dag/devel/3p/IBM4690-TLOG/SA/TestData_SA/EAMTRANA_2.DAT")
+    val file = File("/Users/dag/Downloads/tlog")
     val fis = java.io.FileInputStream(file)
     val dis = InputSourceDataInputStream(fis)
     //
     // Setup JDOM outputter
     //
     val outputter = JDOMInfosetOutputter()
+
+    val res = dp.parse(dis, outputter)
+
+    // Check for errors
+    //
+    val err = res.isError
+    if (err) {
+        // didn't parse the data. Must be diagnostic of some sort.
+        val diags = res.diagnostics
+        for (d in diags) {
+            System.err.println(d.someMessage)
+        }
+        System.exit(2)
+    }
+
+    val doc = outputter.result
+    println(doc)
+
+    val xmlOutputter = XMLOutputter(Format.getPrettyFormat())
+    xmlOutputter.output(doc, System.out)
 }
